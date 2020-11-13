bb6a19256bad2bcdaf39500dd4df772668638262
william clauzel <williamclauzel@MacBook-Pro-de-william.local>
2020-11-13 19:47:28 +0100

    Manage paging

--- src/components/Offer/Offers/index.js
+++ src/components/Offer/Offers/index.js
@@ -2,12 +2,27 @@ import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 import OfferCard from "../OfferCard";
+import Paging from "../../Share/Paging";
 import "./index.css";
 
-const getOffers = async (setOffers, setIsLoading) => {
-  const url = Cookies.get("BackUrl") + "offers";
+const getOffers = async (
+  setOffers,
+  setIsLoading,
+  setCurrentPage,
+  setCountPage,
+  requestedPage
+) => {
+  const pageSize = 8;
+  const url =
+    Cookies.get("BackUrl") +
+    "offers?page=" +
+    requestedPage +
+    "&limit=" +
+    pageSize;
   try {
     const response = await axios.get(url);
+    setCurrentPage(requestedPage);
+    setCountPage(Math.ceil(Number(response.data.count) / pageSize));
     setOffers(response.data.offers);
     setIsLoading(false);
   } catch (error) {
@@ -26,18 +41,34 @@ const getOffers = async (setOffers, setIsLoading) => {
 const Offers = () => {
   const [offers, setOffers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [countPage, setCountPage] = useState(1);
+  const [requestedPage, setRequestedPage] = useState(1);
 
   useEffect(() => {
-    getOffers(setOffers, setIsLoading);
-  }, []);
+    getOffers(
+      setOffers,
+      setIsLoading,
+      setCurrentPage,
+      setCountPage,
+      requestedPage
+    );
+  }, [requestedPage]);
 
   return isLoading ? (
     <div className="offers wrapper">Chargement encours</div>
   ) : (
     <div className="offers wrapper">
-      {offers.map((offer, index) => {
-        return <OfferCard key={index} offer={offer} />;
-      })}
+      <div>
+        {offers.map((offer, index) => {
+          return <OfferCard key={index} offer={offer} />;
+        })}
+      </div>
+      <Paging
+        currentPage={currentPage}
+        countPage={countPage}
+        setRequestedPage={setRequestedPage}
+      />
     </div>
   );
 };
---
+++ src/components/Share/Paging/index.js
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from "react";
+import "./index.css";
+
+const Paging = ({ currentPage, countPage, setRequestedPage }) => {
+  const [pagePreviousValue, setPagePreviousValue] = useState(null);
+  const page1Value = "1";
+  const [page2Value, setPage2Value] = useState(null);
+  const [page3Value, setPage3Value] = useState(null);
+  const [page4Value, setPage4Value] = useState(null);
+  const [page5Value, setPage5Value] = useState(null);
+  const [page6Value, setPage6Value] = useState(null);
+  const [page7Value, setPage7Value] = useState(null);
+  const [page8Value, setPage8Value] = useState(null);
+  const [pageNextValue, setPageNextValue] = useState(null);
+
+  useEffect(() => {
+    const nbPages = Number(countPage) || 1;
+    const crtPage = Number(currentPage) || 1;
+    let ecart = 0;
+    if (nbPages > crtPage) {
+      ecart = nbPages - crtPage > 1 ? 2 : 1;
+    }
+    if (crtPage >= 6) {
+      setPage2Value("...");
+      setPage3Value(crtPage + ecart - 4);
+      setPage4Value(crtPage + ecart - 3);
+      setPage5Value(crtPage + ecart - 2);
+      setPage6Value(crtPage + ecart - 1);
+      setPage7Value(crtPage + ecart);
+      setPage8Value(nbPages - crtPage > 2 ? "..." : null);
+    } else {
+      setPage2Value(crtPage === 2 || crtPage + ecart >= 2 ? 2 : null);
+      setPage3Value(crtPage === 3 || crtPage + ecart >= 3 ? 3 : null);
+      setPage4Value(crtPage === 4 || crtPage + ecart >= 4 ? 4 : null);
+      setPage5Value(crtPage === 5 || crtPage + ecart >= 5 ? 5 : null);
+      setPage6Value(crtPage === 6 || crtPage + ecart >= 6 ? 6 : null);
+      setPage7Value(crtPage === 7 || crtPage + ecart >= 7 ? 7 : null);
+      setPage8Value(nbPages > 7 ? "..." : null);
+    }
+    setPagePreviousValue(crtPage > 1 ? crtPage - 1 : null);
+    setPageNextValue(nbPages > crtPage ? crtPage + 1 : null);
+  }, [currentPage, countPage, setRequestedPage]);
+
+  return (
+    <div className="paging">
+      <div>
+        {pagePreviousValue && (
+          <div
+            className="page"
+            onClick={() => {
+              Number(pagePreviousValue) &&
+                setRequestedPage(Number(pagePreviousValue));
+            }}
+          >
+            {"<"}
+          </div>
+        )}
+        {page1Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page1Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              setRequestedPage(page1Value);
+            }}
+          >
+            {page1Value}
+          </div>
+        )}
+        {page2Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page2Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page2Value) && setRequestedPage(Number(page2Value));
+            }}
+          >
+            {page2Value}
+          </div>
+        )}
+        {page3Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page3Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page3Value) && setRequestedPage(Number(page3Value));
+            }}
+          >
+            {page3Value}
+          </div>
+        )}
+        {page4Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page4Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page4Value) && setRequestedPage(Number(page4Value));
+            }}
+          >
+            {page4Value}
+          </div>
+        )}
+        {page5Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page5Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page5Value) && setRequestedPage(Number(page5Value));
+            }}
+          >
+            {page5Value}
+          </div>
+        )}
+        {page6Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page6Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page6Value) && setRequestedPage(Number(page6Value));
+            }}
+          >
+            {page6Value}
+          </div>
+        )}
+        {page7Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page7Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page7Value) && setRequestedPage(Number(page7Value));
+            }}
+          >
+            {page7Value}
+          </div>
+        )}
+        {page8Value && (
+          <div
+            className={
+              Number(currentPage) === Number(page8Value)
+                ? "page selected"
+                : "page"
+            }
+            onClick={() => {
+              Number(page8Value) && setRequestedPage(Number(page8Value));
+            }}
+          >
+            {page8Value}
+          </div>
+        )}
+      </div>
+      {pageNextValue && (
+        <div
+          className="page"
+          onClick={() => {
+            Number(pageNextValue) && setRequestedPage(Number(pageNextValue));
+          }}
+        >
+          {">"}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Paging;
